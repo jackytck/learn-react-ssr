5cf81b17a5cdd7f753d122c4074990b04b05145b
Jacky Tang <jackytck@gmail.com>
2017-11-13 00:05:48 +0800

    :construction: add UsersList

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,11 +1,13 @@
 import Home from './components/Home'
 import React from 'react'
 import { Route } from 'react-router-dom'
+import UsersList from './components/UsersList'
 
 export default () => {
   return (
     <div>
       <Route exact path='/' component={Home} />
+      <Route path='/users' component={UsersList} />
     </div>
   )
 }
