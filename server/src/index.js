2bd996a3160ae5e969113659953fd386b4a141d1
Jacky Tang <jackytck@gmail.com>
2017-11-19 00:02:07 +0800

    :sparkles: api proxy for client side

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,17 @@ import Routes from './client/Routes'
 import createStore from './helpers/createStore'
 import express from 'express'
 import { matchRoutes } from 'react-router-config'
+import proxy from 'express-http-proxy'
 import renderer from './helpers/renderer'
 
 const app = express()
 
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
+  proxyReqOptDecorator(opts) {
+    opts.header['x-forwarded-host'] = 'localhost:3000'
+    return opts
+  }
+}))
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
