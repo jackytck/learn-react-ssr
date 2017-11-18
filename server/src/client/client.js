f412676241d801530abf989c883126f95cf418ee
Jacky Tang <jackytck@gmail.com>
2017-11-19 00:02:40 +0800

    :hammer: use customized axios for client request

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -8,11 +8,20 @@ import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './Routes'
+import axios from 'axios'
 import reducers from './reducers'
 import { renderRoutes } from 'react-router-config'
 import thunk from 'redux-thunk'
 
-const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk))
+const axiosInstance = axios.create({
+  baseURL: '/api'
+})
+
+const store = createStore(
+  reducers,
+  window.INITIAL_STATE,
+  applyMiddleware(thunk.withExtraArgument(axiosInstance))
+)
 
 ReactDOM.hydrate(
   <Provider store={store}>
