8c577680026a45cd630cef80c1e65a2638a90e2b
Jacky Tang <jackytck@gmail.com>
2017-11-20 00:15:02 +0800

    :hammer: use customized axios for server request

--- server/src/helpers/createStore.js
+++ server/src/helpers/createStore.js
@@ -1,10 +1,20 @@
 import { applyMiddleware, createStore } from 'redux'
 
+import axios from 'axios'
 import reducers from '../client/reducers'
 import thunk from 'redux-thunk'
 
-export default () => {
-  const store = createStore(reducers, {}, applyMiddleware(thunk))
+export default req => {
+  const axiosInstance = axios.create({
+    baseURL: 'http://react-ssr-api.herokuapp.com',
+    headers: { cookie: req.get('cookie') || '' }
+  })
+
+  const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(thunk.withExtraArgument(axiosInstance))
+  )
 
   return store
 }
