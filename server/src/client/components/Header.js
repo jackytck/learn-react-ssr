94cf3131f45bea028e4c6357a953ebcf30d7531d
Jacky Tang <jackytck@gmail.com>
2017-11-23 00:08:16 +0800

    :sparkles: add login + logout in Header

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -1,10 +1,30 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default () => {
+const Header = ({ auth }) => {
+  const authButton = auth ? (
+    <a href='/api/logout'>Logout</a>
+  ) : (
+    <a href='/api/auth/google'>Login</a>
+  )
+
   return (
-    <div>
-      <Link to='/'>React SSR</Link>
-    </div>
+    <nav>
+      <div className='nav-wrapper'>
+        <Link to='/' className='brand-logo'>React SSR</Link>
+        <ul className='right'>
+          <li><Link to='/users'>Users</Link></li>
+          <li><Link to='/admins'>Admins</Link></li>
+          <li>{authButton}</li>
+        </ul>
+      </div>
+    </nav>
   )
 }
+
+function mapStateToProps ({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
